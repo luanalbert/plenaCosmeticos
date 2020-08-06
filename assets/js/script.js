cde7d3ba51fd599dd63ff79f401d9aa84fa0c3ae
luanalbert <contatoluanalbert@gmail.com>
2020-08-06 11:41:19 -0300

    finalização

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".body-card0").hover(
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".btn-group0").addClass("btn-bg-hover");
+         $(".teste").html('Comprar');
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".btn-group").removeClass("btn-bg-hover");
+      });
+});
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".body-card1").hover(   
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".btn-group1").addClass("btn-bg-hover");
+         $(".teste").html('Comprar');
+         
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".btn-group1").removeClass("btn-bg-hover");
+      });
+});
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".body-card2").hover(
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".btn-group2").addClass("btn-bg-hover");
+         $(".teste").html('Comprar');
+         
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".btn-group2").removeClass("btn-bg-hover");
+      });
+});
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".body-card3").hover(
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".btn-group3").addClass("btn-bg-hover");
+         $(".teste").html('Comprar');
+         
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".btn-group3").removeClass("btn-bg-hover");
+      });
+});
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".body-card4").hover(
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".btn-group4").addClass("btn-bg-hover");
+         $(".teste").html('Comprar');
+         
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".btn-group4").removeClass("btn-bg-hover");
+      });
+});
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".body-card5").hover(
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".btn-group5").addClass("btn-bg-hover");
+         $(".teste").html('Comprar');
+         
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".btn-group5").removeClass("btn-bg-hover");
+      });
+});
+
